a174cf48a82e58dd7c59e396247ca421b3209368
Jon Jensen <jon@instructure.com>
2014-06-28 13:39:08 -0600

    looks like we don't need to do this after all
    may need to revisit this if there are edge cases we aren't considering...
    see https://github.com/DatabaseCleaner/database_cleaner/issues/99

--- lib/onceler/transactions/active_record_3.rb
+++ lib/onceler/transactions/active_record_3.rb
@@ -1,9 +1,6 @@
 module Onceler
   module Transactions
     def begin_transaction(conn)
-      unless conn.instance_variable_get(:@_current_transaction_records)
-        conn.instance_variable_set(:@_current_transaction_records, [])
-      end
       if conn.open_transactions == 0
         conn.transaction_joinable = false
         conn.begin_db_transaction
@@ -17,10 +14,8 @@ module Onceler
       conn.decrement_open_transactions
       if conn.open_transactions == 0
         conn.rollback_db_transaction
-        conn.send :rollback_transaction_records, true
       else
         conn.rollback_to_savepoint
-        conn.send :rollback_transaction_records, false
       end
     end
   end
